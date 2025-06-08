1f18b5490d8ed324aecbcb4631c0dcca8954b495
campbellsamuelryan <campbellsamuelryan@gmail.com>
2025-06-08 16:00:24 -0400

    Add files via upload

--- lab04/postlab/bitCounter.cpp
+++ lab04/postlab/bitCounter.cpp
@@ -1,26 +1,69 @@
-// Dion Niazi dn3gy 14 02 2017 bitCounter.cpp
-#include <iostream>
-using namespace std;
-int numOfOnes(int n);
-int main(int argc, char **argv)
-{
-  if(argc!=2){
-    cout<<" You need to enter one parameter!"<<endl;
-    exit(1);
-  }
-  int num = atof(argv[1]);
-  cout<<numOfOnes(num)<<endl;
-}
+//Samuel Campbell
+//CS2150 'bitCounter.cpp'
 
-// recursive function that returns the number of 1's in the binary representation of n
-int numOfOnes(int n)
+#include <iostream>
+
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int bitCounter(int n)
 {
-  if(n==0){
-    return 0;
-  }
-  else if(n==1){
+  if (n / 2 == 0)
+  {
     return 1;
   }
+  else if (n % 2 == 0)
+  {
+    return bitCounter(n / 2);
+  }
   else
-    return numOfOnes(n/2)+numOfOnes(n%2);
+  {
+    return 1 + bitCounter(n / 2);
+  }
+}
+
+string baseConverter(int num, int base ){
+  int dec=0;
+  string subResult="";
+  string result = "";
+   if (num == 0){
+    return "0";
+   }
+
+   while(num){
+    int rem  = num % (base);
+    num /= (base);
+    if(rem < 0) {
+
+      rem += num;
+      num++;
+
+    }
+    subResult += to_string(rem);
+
+   }
+   
+    for (int i = subResult.length() - 1; i >= 0; i--){
+        result.push_back(subResult[i]);
+    }
+
+   
+   return result;
+}
+
+int main(int argc, char **argv)
+{
+
+  int bitCount = atoi(argv[1]);
+  cout << bitCount << " has " << bitCounter(bitCount) << " bit(s)" << endl;
+
+  string num = string(argv[2]);
+  int start = atoi(argv[3]);
+  int end = atoi(argv[4]);
+  cout << num << " (base " << start << ") "
+       << "= " << baseConverter(stoi(num), end) << " (base " << end << ")" << endl;
+
+  return 0;
 }
